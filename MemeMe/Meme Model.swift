2b71f346f01992a81ef2fe560e6f31c5ecdcb5f6
Hank Wang <drapho@gmail.com>
2018-05-03 20:56:57 +0900

    add Meme Model

---
+++ MemeMe/Meme Model.swift
@@ -0,0 +1,17 @@
+//
+//  Meme Model.swift
+//  MemeMe
+//
+//  Created by Hank Wang on 2018/5/3.
+//  Copyright © 2018 hanksudo. All rights reserved.
+//
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage?
+}
+
+
